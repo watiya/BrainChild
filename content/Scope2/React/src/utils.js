1743229995ad56e3e6c1e0eb8a4531f06db6d0ef
watiya <blingblingjiyu@gmail.com>
2022-03-29 23:47:29 +0900

    modify

--- content/Scope2/React/src/utils.js
+++
@@ -1,9 +0,0 @@
-export const numberWithCommas = (x) => {
-  var parts = x.toString().split(".");
-  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  return parts.join(".");
-};
-
-export const progress = () => {
-  return Math.floor(Math.random() * 90) + 10 + "%";
-};
