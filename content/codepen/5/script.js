c6e2151aac461fdcdb9953bf6bdc7f3101f6d044
watiya <blingblingjiyu@gmail.com>
2023-02-28 14:29:43 +0900

    update

---
+++ content/codepen/5/script.js
@@ -0,0 +1,71 @@
+// Resources by pikisuperstar /freepic
+// inspired by phlox.pro
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+gsap .to("#bg" ,{
+    scrollTrigger:{
+        scrub:2
+    },
+    y:30,
+    
+    
+});
+gsap .to("#group" ,{
+    scrollTrigger:{
+        scrub:1
+    },
+    y:50,
+    
+});
+gsap .to("#hb" ,{
+    scrollTrigger:{
+        scrub:1
+    },
+    rotate:50,
+    
+});
+gsap .to("#hb2" ,{
+    scrollTrigger:{
+        scrub:1
+    },
+    y:-50,
+    
+});
+gsap .to("#b1" ,{
+    scrollTrigger:{
+        scrub:1
+    },
+    y:-50,
+    
+});
+gsap .to("#b2" ,{
+    scrollTrigger:{
+        scrub:1
+    },
+    x:-70,
+    
+});
+gsap .to("#b3" ,{
+    scrollTrigger:{
+        scrub:1
+    },
+    y:-70,
+    
+});
+gsap .to("#dots1" ,{
+    scrollTrigger:{
+        scrub:1
+    },
+    rotate:100,
+    
+});
+gsap .to("#dots2" ,{
+    scrollTrigger:{
+        scrub:1
+    },
+    rotate:100,
+    
+});
